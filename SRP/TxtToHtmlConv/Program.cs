99e903215e821a8f9f06f6e46b008f4fe52d13fe
Cppshizoid <alex.krivoschecov@gmail.com>
2023-01-09 11:18:05 +0700

    Create Program.cs

---
+++ SRP/TxtToHtmlConv/Program.cs
@@ -0,0 +1,17 @@
+using TextToHtmlConvertor;
+
+try
+{
+    Console.WriteLine("Please specify the file to convert to HTML.");
+    var fullFilePath = Console.ReadLine();
+    var fileProcessor = new FileProcessor(fullFilePath);
+    var textProcessor = new TextProcessor(fileProcessor);
+    textProcessor.ConvertText();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
